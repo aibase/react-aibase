3ebcbd39235485d2201dbabc15dc74dfcfe6fb20
stefian <v_ianta@yahoo.com>
2019-10-22 09:45:51 -0300

    added localhost link for the REST endpoint for getting a post / view

--- src/pages/Feed/SinglePost/SinglePost.js
+++ src/pages/Feed/SinglePost/SinglePost.js
@@ -14,7 +14,7 @@ class SinglePost extends Component {
 
   componentDidMount() {
     const postId = this.props.match.params.postId;
-    fetch('URL')
+    fetch('http://localhost:8081/feed/post/' + postId)
       .then(res => {
         if (res.status !== 200) {
           throw new Error('Failed to fetch status');
