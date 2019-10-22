f38251fdec3ce6fd108d66df5e6937060a5577fa
stefian <v_ianta@yahoo.com>
2019-10-22 15:25:29 -0300

    added local url path for deleting a post

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -176,7 +176,9 @@ class Feed extends Component {
 
   deletePostHandler = postId => {
     this.setState({ postsLoading: true });
-    fetch('URL')
+    fetch('http://localhost:8081/feed/post/' + postId, {
+      method: 'DELETE'
+    })
       .then(res => {
         if (res.status !== 200 && res.status !== 201) {
           throw new Error('Deleting a post failed!');
