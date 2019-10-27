2cc84208164aef6c83cdd19b58548b8ecf739cae
stefian <v_ianta@yahoo.com>
2019-10-27 10:31:23 -0300

    added updatePost function and socket.io event listener

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -44,6 +44,8 @@ class Feed extends Component {
     socket.on('posts', data => {
       if (data.action === 'create') {
         this.addPost(data.post);
+      } else if (data.action === 'update') {
+        this.updatePost(data.post);
       }
     })
   }
@@ -60,6 +62,19 @@ class Feed extends Component {
       return {
         posts: updatedPosts,
         totalPosts: prevState.totalPosts + 1
+      };
+    });
+  };
+
+  updatePost = post => {
+    this.setState(prevState => {
+    	const updatedPosts = [...prevState.posts];
+		  const updatedPostIndex = updatedPosts.findIndex(p => p._id === post._id);
+      if (updatedPostIndex > -1) {
+        updatedPosts[updatedPostIndex] = post;
+      }
+      return {
+        posts: updatedPosts
       };
     });
   };
@@ -186,17 +201,17 @@ class Feed extends Component {
           createdAt: resData.post.createdAt
         };
         this.setState(prevState => {
-          let updatedPosts = [...prevState.posts];
-          if (prevState.editPost) {
-            const postIndex = prevState.posts.findIndex(
-              p => p._id === prevState.editPost._id
-            );
-            updatedPosts[postIndex] = post;
-          } else if (prevState.posts.length < 2) {
-            updatedPosts = prevState.posts.concat(post);
-          }
+          // let updatedPosts = [...prevState.posts];
+          // if (prevState.editPost) {
+          //   const postIndex = prevState.posts.findIndex(
+          //     p => p._id === prevState.editPost._id
+          //   );
+          //   updatedPosts[postIndex] = post;
+          // } else if (prevState.posts.length < 2) {
+          //   updatedPosts = prevState.posts.concat(post);
+          // }
           return {
-            posts: updatedPosts,
+            // posts: updatedPosts,
             isEditing: false,
             editPost: null,
             editLoading: false
